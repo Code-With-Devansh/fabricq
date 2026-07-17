6066551165eec6dd355c5e404f2563d98c026ceb
Code-With-Devansh <95998107+Code-With-Devansh@users.noreply.github.com>
2026-07-17 18:18:43 +0530

    Refactor uploadJob function and update job-related Postman configurations

--- src/controller/jobs.controller.js
+++ src/controller/jobs.controller.js
@@ -2,17 +2,6 @@ import { createJobService } from "../services/httpJob.js";
 import { createJobSchema } from "../validators/http_job.js";
 import { asyncHandler } from "../middlewares/asyncHandler.js";
 import {AppError} from "../Error/appError.js"
-export const uploadJob = asyncHandler(async (req, res) => {
-  const payload = req.body;
-  const validated = createJobSchema.safeParse(payload);
-
-  if (!validated.success) {
-    throw new AppError("Validation failed", 400, validated.error.flatten());
-  }
-
-  const job = await createJobService(validated.data);
-  return res.status(201).json({ success: true, data: job });
-});
 import {
   getJobsService,
   getJobByIdService,
@@ -26,6 +15,19 @@ import {
   listJobsQuerySchema,
   paginationQuerySchema,
 } from "../validators/http_job.js";
+
+
+export const uploadJob = asyncHandler(async (req, res) => {
+  const payload = req.body;
+  const validated = createJobSchema.safeParse(payload);
+
+  if (!validated.success) {
+    throw new AppError("Validation failed", 400, validated.error.flatten());
+  }
+
+  const job = await createJobService(validated.data);
+  return res.status(201).json({ success: true, data: job });
+});
 
 export const getJobs = asyncHandler(async (req, res) => {
   const validated = listJobsQuerySchema.safeParse(req.query);
